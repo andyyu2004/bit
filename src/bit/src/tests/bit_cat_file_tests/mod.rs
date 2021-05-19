b36749ef0c226994d1542689866f50fd3943b48f
Andy Yu <andyyu2004@gmail.com>
2021-05-19 17:43:58 +1200

    readd foo repo in bit as it thought it was a submodule
    move bit/src/tests/repos to just bit/test/repos

--- src/bit/src/tests/bit_cat_file_tests/mod.rs
+++ src/bit/src/tests/bit_cat_file_tests/mod.rs
@@ -7,16 +7,16 @@ author Andy Yu <andyyu2004@gmail.com> 1621330843 +1200
 committer Andy Yu <andyyu2004@gmail.com> 1621330907 +1200
 
 init"#;
-    bit!("-C src/tests/repos/foo cat-file -p HEAD").stdout(expected);
+    bit!("-C tests/repos/foo cat-file -p HEAD").stdout(expected);
 }
 
 #[test]
 fn test_cli_cat_file_pretty_commit_output() {
-    bit!("-C src/tests/repos/foo cat-file -p bc01ca0eb625f386c396605d7813be3e522df1c0")
+    bit!("-C tests/repos/foo cat-file -p bc01ca0eb625f386c396605d7813be3e522df1c0")
         .stdout("qux content\n");
 }
 
 #[test]
 fn test_cli_cat_file_partial_oid() {
-    bit!("-C src/tests/repos/foo cat-file -p bc01ca0").stdout("qux content\n");
+    bit!("-C tests/repos/foo cat-file -p bc01ca0").stdout("qux content\n");
 }
